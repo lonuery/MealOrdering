54993d9d92b96b007d372c39738c4bc4ce590654
lonuery <lonuery@163.com>
2014-09-17 14:12:09 +0800

    添加了一个主页面

---
+++ MealOrdering/src/com/lonuery/main/MainPage.java
@@ -0,0 +1,18 @@
+package com.lonuery.main;
+
+import com.lonuery.mealordering.R;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainPage extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_page);
+		
+		
+	}
+	
+	
+}
